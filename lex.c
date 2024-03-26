d8ccbb9a53118c8d2cab38ce34f242a347e5825f
tsegaye27 <ttalegn29@gmail.com>
2024-03-26 22:38:09 +0300

    made some adjustments

--- lex.c
+++ lex.c
@@ -20,6 +20,44 @@ typedef struct {
     char lexeme[100];
 } Token;
 
+// C# Keywords
+const char* keywords[] = {
+    "abstract", "as", "base", "bool", "break", "byte", "case", "catch",
+    "char", "checked", "class", "const", "continue", "decimal", "default",
+    "delegate", "do", "double", "else", "enum", "event", "explicit",
+    "extern", "false", "finally", "fixed", "float", "for", "foreach",
+    "goto", "if", "implicit", "in", "int", "interface", "internal", "is",
+    "lock", "long", "namespace", "new", "null", "object", "operator", "out",
+    "override", "params", "private", "protected", "public", "readonly",
+    "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc",
+    "static", "string", "struct", "switch", "this", "throw", "true", "try",
+    "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using",
+    "using static", "virtual", "void", "volatile", "while"
+};
+
+const char* operators[] = {
+    "+", "-", "*", "/", "%",
+    "=", "+=", "-=", "*=", "/=", "%=",
+    "<<=", ">>=", "&=", "^=", "|=",
+    "==", "!=", "<", ">", "<=", ">=",
+    "&&", "||", "!",
+    "&", "|", "^", "~", "<<", ">>",
+    "++", "--",
+    "?", ":", ".", "->",
+    "&"
+};
+
+const char* tokenTypeNames[] = {
+    "IDENTIFIER",
+    "KEYWORD",
+    "NUMBER",
+    "STRING",
+    "OPERATOR",
+    "PUNCTUATION",
+    "UNKNOWN"
+};
+
+
 // Lexical analysis function
 Token getNextToken(FILE* file) {
     Token token;
@@ -47,21 +85,19 @@ Token getNextToken(FILE* file) {
         token.lexeme[i] = '\0';
 
         // Check if it's a keyword
-        if (strcmp(token.lexeme, "if") == 0 ||
-            strcmp(token.lexeme, "else") == 0 ||
-            strcmp(token.lexeme, "while") == 0 ||
-            strcmp(token.lexeme, "int") == 0 ||
-            strcmp(token.lexeme, "float") == 0 ||
-            strcmp(token.lexeme, "string") == 0) {
-            token.type = TOKEN_KEYWORD;
-        } else {
-            token.type = TOKEN_IDENTIFIER;
+        int numKeywords = sizeof(keywords) / sizeof(keywords[0]);
+        for (int j = 0; j < numKeywords; j++) {
+            if (strcmp(token.lexeme, keywords[j]) == 0) {
+                token.type = TOKEN_KEYWORD;
+                return token;
+            }
         }
+
+        token.type = TOKEN_IDENTIFIER;
 
         ungetc(currentChar, file); // Push back the last character
         return token;
     }
-
     // Number
     if (isdigit(currentChar)) {
         int i = 0;
@@ -105,12 +141,17 @@ Token getNextToken(FILE* file) {
     }
 
     // Operators
-    if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
-        token.lexeme[0] = currentChar;
-        token.lexeme[1] = '\0';
-        token.type = TOKEN_OPERATOR;
-        return token;
+   int numOperators = sizeof(operators) / sizeof(operators[0]);
+
+    for (int i = 0; i < numOperators; i++) {
+        if (currentChar == operators[i][0] && operators[i][1] == '\0') {
+            token.lexeme[0] = currentChar;
+            token.lexeme[1] = '\0';
+            token.type = TOKEN_OPERATOR;
+            return token;
+        }
     }
+
 
     // Punctuation
     if (currentChar == '(' || currentChar == ')' || currentChar == '{' || currentChar == '}' || currentChar == ';') {
@@ -140,7 +181,7 @@ int main() {
         token = getNextToken(file);
 
         // Print token information
-        printf("Type: %d, Lexeme: %s\n", token.type, token.lexeme);
+        printf("Type: %s, Lexeme: %s\n", tokenTypeNames[token.type], token.lexeme);
     } while (token.type != TOKEN_UNKNOWN);
 
     fclose(file);
