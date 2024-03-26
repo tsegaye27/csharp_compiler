b5f57f0aff9c1ff44b4e9bed7ad7ca9bc6304af3
tsegaye27 <ttalegn29@gmail.com>
2024-03-26 21:29:37 +0300

    initial commit

---
+++ source.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+
+        Console.WriteLine("The sum of {0} and {1} is: {2}", num1, num2, sum);
+    }
+}
